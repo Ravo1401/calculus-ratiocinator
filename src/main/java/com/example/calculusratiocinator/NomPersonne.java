6e691b2b568af174585cd413a8068154cc7f9673
Ravo1401 <hei.ravo@gmail.com>
2024-06-07 15:34:45 +0200

    feat: add test LouEstPauvreEtGénéreux and test LouEstBeauDoncPauvre

--- src/main/java/com/example/calculusratiocinator/NomPersonne.java
+++ src/main/java/com/example/calculusratiocinator/NomPersonne.java
@@ -2,4 +2,8 @@ package com.example.calculusratiocinator;
 
 public class NomPersonne {
     public String nom;
+
+    public NomPersonne(String nom) {
+        this.nom = nom;
+    }
 }
--- src/test/java/com/example/calculusratiocinator/TesteAffirmation.java
+++ src/test/java/com/example/calculusratiocinator/TesteAffirmation.java
@@ -3,14 +3,16 @@ package com.example.calculusratiocinator;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import static org.junit.jupiter.api.Assertions.assertFalse;
+
 @SpringBootTest
 public class TesteAffirmation {
     @Test
     void LouEstPauvreEtGénéreux () {
         var Lou = new NomPersonne("Lou");
-        var Affirmation = new Affirmation("true");
+        var Genereux = new Genereux(true);
         var Pauvre = new Pauvre();
-        assertFalse(LogiqueEtOu.Et(Pauvre.evaluation(), Affirmation.evaluation()));
+        assertFalse(LogiqueEtOu.Et(Pauvre.evaluation(), Genereux.evaluation()));
     }
 
     @Test
@@ -18,6 +20,6 @@ public class TesteAffirmation {
         var lou = new NomPersonne("Lou");
         var Beau = new Beau();
         var Pauvre = new Pauvre();
-        assertFalse(LogiqueEtOu.E)
+        assertFalse(LogiqueEtOu.Et(Pauvre.evaluation(), Beau.evaluation()));
     }
 }
